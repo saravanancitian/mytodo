6c94b4595bbdc04ad7309ad327359008906e6f7e
saravanancitian <saravanancitian@gmail.com>
2022-09-25 22:48:39 +0530

    moved db function to seprate file

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -13,49 +11,6 @@ type user struct {
 	Name     string `json:"name,omitempty"`
 	Email    string `json:"email,omitempty"`
 	Password string `json:"password,omitempty"`
-}
-
-var db *sql.DB
-var insert_user *sql.Stmt
-
-func openDb() {
-	var err error
-	db, err = sql.Open("mysql", "root:root@/mytodo")
-	if err != nil {
-		panic(err)
-	}
-	db.SetMaxOpenConns(1)
-	insert_user, err = db.Prepare("insert into user( name, email, password) values(?,?,?)")
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
-}
-
-func closeDb() {
-	insert_user.Close()
-	db.Close()
-}
-
-func testdb() {
-	// result, err := insert_user.Exec("testusr", "testuser@test.com", "password")
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// fmt.Println(result.LastInsertId())
-
-	rows, err := db.Query("select * from user")
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var usr user
-		rows.Scan(&usr.Userid, &usr.Name, &usr.Email, &usr.Password)
-		fmt.Printf("%s %s %s", usr.Name, usr.Email, usr.Password)
-	}
-
 }
 
 // func testform(w http.ResponseWriter, r *http.Request) {
@@ -77,6 +32,8 @@ func testdb() {
 func main() {
 	openDb()
 	iniweb()
+
+	http.Handle("/js", http.FileServer(http.Dir("static/js/")))
 
 	http.HandleFunc("/", indexhtml)
 	http.HandleFunc("/signin.html", signinhtml)
